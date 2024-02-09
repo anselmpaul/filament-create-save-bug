3532e8c63f17db4c2f87907eb1209c528a95bdfc
Bunsen <anselm.bunsen@sandstorm.de>
2024-02-09 15:48:10 +0100

    initial commit

---
+++ app/Filament/Resources/TestResource/Pages/CreateTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\TestResource\Pages;
+
+use App\Filament\Resources\TestResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTest extends CreateRecord
+{
+    protected static string $resource = TestResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            parent::getCreateFormAction(),
+            parent::getCancelFormAction(),
+        ];
+    }
+}
